dfd2632c78ff1db8d7b7deea96ac0cbf4a135cbb
Huanshuo (Happy) Wang <huanshuowang1210@gmail.com>
2026-04-25 14:41:59 -0700

    Delete github website directory （搞错了）

--- github website/github website/js/loading-redirect.js
+++
@@ -1,8 +0,0 @@
-// Check if this is the user's first visit
-if (!localStorage.getItem('hasVisited')) {
-    // Set the flag to indicate the user has visited
-    localStorage.setItem('hasVisited', 'true');
- 
-    // Redirect to the loading page
-    window.location.href = 'loading.html';
-} 
